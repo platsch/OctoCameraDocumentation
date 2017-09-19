a6faca368c6261f9a4bb4c9a858ba510eb4fe4dc
3struhs <3struhs@informatik.uni-hamburg.de>
2017-09-19 15:48:09 +0200

    Added code for Camera lookup
    Added first version of the code for the 3d Printer camera lookup process.

--- octoprint_OctoCamDox/__init__.py
+++ octoprint_OctoCamDox/__init__.py
@@ -34,6 +34,7 @@ import json
 import struct
 import imghdr
 
+from collections import deque
 from .GCode_processor import CameraGCodeExtraction as GCodex
 from .GCode_processor import CustomJSONEncoder as CoordJSONify
 from .CameraCoordinateGetter import CameraGridMaker
@@ -72,6 +73,7 @@ class OctoCamDox(octoprint.plugin.StartupPlugin,
         self.currentLayer = 0
 
         self.cameraImagePath = None
+        self.qeue = None
 
         self.CamPixelX = None
         self.CamPixelY = None
@@ -164,9 +166,8 @@ class OctoCamDox(octoprint.plugin.StartupPlugin,
             dir_name = self._settings.global_get_basefolder("uploads")
             base_filename = payload.get("path")
             uploadsPath = os.path.join(dir_name, base_filename)
-            #f = open(payload.get("file"),'r')
+
             f = self._openGCodeFiles(uploadsPath)
-            #f = open(testPath, 'r')
 
             #Extract the GCodes for the CameraPath Algortihm
             newCamExtractor.extractCameraGCode(f)
@@ -227,16 +228,11 @@ class OctoCamDox(octoprint.plugin.StartupPlugin,
 
             # switch to pimary extruder, since the head camera is relative to this extruder and the offset to PNP nozzle might not be known (firmware offset)
             self._printer.commands("T0")
-            self._printer.commands("G1 Z" + str(self._currentZ+5) + " F" + str(self.FEEDRATE)) # lift printhead
+            # Create the qeue for the printer camera coordinates
+            self.qeue = deque(self.CameraGridCoordsList[self.currentLayer])
+            elem = self.getNewQeueElem()
+            self.get_camera_image(elem.x, elem.y, self.get_camera_image_callback, True)
 
-            for eachItem in self.CameraGridCoordsList[self.currentLayer]:
-                # move camera to grid position
-                self._logger.info( "Move camera to position X: %s Y: %s", str(eachItem.x), str(eachItem.y))
-                cmd = "G1 X" + str(eachItem.x) + " Y" + str(eachItem.y) + " F" + str(self.FEEDRATE)
-                self._printer.commands(cmd)
-
-            self.currentLayer += 1 #Increment layer
-            self._printer.commands("G1 Z" + str(self._currentZ-5) + " F" + str(self.FEEDRATE)) # lower printhead
             return "G4 P1" # return dummy command
 
     	if "M945" in cmd:
@@ -247,6 +243,16 @@ class OctoCamDox(octoprint.plugin.StartupPlugin,
     	print "returned image path: "
     	print path
         self.cameraImagePath = path
+        elem = self.getNewQeueElem()
+        if(elem):
+            self.get_camera_image(elem.x, elem.y, self.get_camera_image_callback, False)
+
+    def getNewQeueElem(self):
+        if(self.qeue):
+            return self.qeue.popleft()
+        else:
+            self.currentLayer += 1 #Increment layer when qeue was empty
+            return(None)
 
     def _openGCodeFiles(self, inputName):
         gcode = open( inputName, 'r' )
